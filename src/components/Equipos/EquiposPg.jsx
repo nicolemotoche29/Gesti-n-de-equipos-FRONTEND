0a6e433ac0288f52d5a842941e8eeb1bb9be1942
nicolemotoche29 <fernandamotoche@gmail.com>
2024-07-03 22:45:11 -0500

    CRUD Equipos

--- src/components/Equipos/EquiposPg.jsx
+++ src/components/Equipos/EquiposPg.jsx
@@ -17,7 +17,20 @@ const EquiposPg = () => {
         area: '',
         idsupus: null
     });
-    const [equipoActualizar, setEquipoActualizar] = useState(null);
+    const [equipoActualizar, setEquipoActualizar] = useState({
+        idcod: '',
+        descripcion: '',
+        marca: '',
+        modelos: '',
+        nserie: '',
+        accesorios: '',
+        fabricante: '',
+        caracteristicas: '',
+        con_instalacion: '',
+        con_utilizacion: '',
+        area: '',
+        idsupus: null
+    });
     const [mostrarFormularioActualizar, setMostrarFormularioActualizar] = useState(false);
     const [idEquipoEliminar, setIdEquipoEliminar] = useState('');
 
@@ -70,7 +83,20 @@ const EquiposPg = () => {
     };
 
     const prepararActualizarEquipo = (equipo) => {
-        setEquipoActualizar(equipo);
+        setEquipoActualizar({
+            idcod: equipo.idcod,
+            descripcion: equipo.descripcion,
+            marca: equipo.marca,
+            modelos: equipo.modelos,
+            nserie: equipo.nserie,
+            accesorios: equipo.accesorios,
+            fabricante: equipo.fabricante,
+            caracteristicas: equipo.caracteristicas,
+            con_instalacion: equipo.con_instalacion,
+            con_utilizacion: equipo.con_utilizacion,
+            area: equipo.area,
+            idsupus: equipo.idsupus
+        });
         setMostrarFormularioActualizar(true);
     };
 
@@ -78,7 +104,7 @@ const EquiposPg = () => {
         e.preventDefault();
         try {
             const token = localStorage.getItem('token');
-            const response = await axios.put(`${import.meta.env.VITE_URL_BACKEND}/equipos/${equipoActualizar.id}`, equipoActualizar, {
+            const response = await axios.put(`${import.meta.env.VITE_URL_BACKEND}/equipos/${equipoActualizar.idcod}`, equipoActualizar, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -86,7 +112,20 @@ const EquiposPg = () => {
             console.log('Equipo actualizado:', response.data);
             obtenerEquipos();
             setMostrarFormularioActualizar(false);
-            setEquipoActualizar(null);
+            setEquipoActualizar({
+                idcod: '',
+                descripcion: '',
+                marca: '',
+                modelos: '',
+                nserie: '',
+                accesorios: '',
+                fabricante: '',
+                caracteristicas: '',
+                con_instalacion: '',
+                con_utilizacion: '',
+                area: '',
+                idsupus: null
+            });
         } catch (error) {
             console.error('Error al actualizar el equipo:', error);
         }
@@ -102,14 +141,14 @@ const EquiposPg = () => {
             });
             console.log('Equipo eliminado:', response.data);
             obtenerEquipos();
-            setIdEquipoEliminar(''); // Limpiar el campo de entrada después de eliminar
+            setIdEquipoEliminar('');
         } catch (error) {
             console.error('Error al eliminar el equipo:', error);
         }
     };
-    
+
     const handleChange = (e) => {
-        setIdEquipoEliminar(e.target.value); // Actualizar el estado con el valor del input
+        setIdEquipoEliminar(e.target.value);
     };
 
     const handleChangeActualizar = (e) => {
@@ -118,6 +157,7 @@ const EquiposPg = () => {
             [e.target.name]: e.target.value
         });
     };
+
     return (
         <div>
             <h1>Equipos</h1>
@@ -128,7 +168,7 @@ const EquiposPg = () => {
                     type="text"
                     name="idcod"
                     value={nuevoEquipo.idcod}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, idcod: e.target.value })}
                     placeholder="Código ID"
                     required
                 />
@@ -136,7 +176,7 @@ const EquiposPg = () => {
                     type="text"
                     name="descripcion"
                     value={nuevoEquipo.descripcion}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, descripcion: e.target.value })}
                     placeholder="Descripción"
                     required
                 />
@@ -144,7 +184,7 @@ const EquiposPg = () => {
                     type="text"
                     name="marca"
                     value={nuevoEquipo.marca}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, marca: e.target.value })}
                     placeholder="Marca"
                     required
                 />
@@ -152,7 +192,7 @@ const EquiposPg = () => {
                     type="text"
                     name="modelos"
                     value={nuevoEquipo.modelos}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, modelos: e.target.value })}
                     placeholder="Modelos"
                     required
                 />
@@ -160,7 +200,7 @@ const EquiposPg = () => {
                     type="text"
                     name="nserie"
                     value={nuevoEquipo.nserie}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, nserie: e.target.value })}
                     placeholder="Nº de serie"
                     required
                 />
@@ -168,7 +208,7 @@ const EquiposPg = () => {
                     type="text"
                     name="accesorios"
                     value={nuevoEquipo.accesorios}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, accesorios: e.target.value })}
                     placeholder="Accesorios"
                     required
                 />
@@ -176,7 +216,7 @@ const EquiposPg = () => {
                     type="text"
                     name="fabricante"
                     value={nuevoEquipo.fabricante}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, fabricante: e.target.value })}
                     placeholder="Fabricante"
                     required
                 />
@@ -184,7 +224,7 @@ const EquiposPg = () => {
                     type="text"
                     name="caracteristicas"
                     value={nuevoEquipo.caracteristicas}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, caracteristicas: e.target.value })}
                     placeholder="Caracteristicas"
                     required
                 />
@@ -192,7 +232,7 @@ const EquiposPg = () => {
                     type="text"
                     name="con_instalacion"
                     value={nuevoEquipo.con_instalacion}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_instalacion: e.target.value })}
                     placeholder="Instalación"
                     required
                 />
@@ -200,7 +240,7 @@ const EquiposPg = () => {
                     type="text"
                     name="con_utilizacion"
                     value={nuevoEquipo.con_utilizacion}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_utilizacion: e.target.value })}
                     placeholder="Utilización"
                     required
                 />
@@ -208,15 +248,16 @@ const EquiposPg = () => {
                     type="text"
                     name="area"
                     value={nuevoEquipo.area}
-                    onChange={handleChange}
+                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, area: e.target.value })}
                     placeholder="Area"
                     required
                 />
                 
                 <button type="submit">Crear Equipo</button>
             </form>
+
             <h2>Actualizar equipo</h2>
-            <br></br>
+            
             {/* Formulario para actualizar un equipo */}
             {mostrarFormularioActualizar && (
                 <form onSubmit={actualizarEquipo}>
@@ -312,24 +353,23 @@ const EquiposPg = () => {
                     <button type="submit">Actualizar Equipo</button>
                 </form>
             )}
-            {/* Campo para ingresar el ID del equipo a eliminar */}
-            
+
             {/* Lista de equipos */}
             <ul>
                 {equipos.map((equipo) => (
-                    <li key={equipo.id}>
+                    <li key={equipo.idcod}>
                         <p>{equipo.idcod} - {equipo.descripcion}</p>
                         {/* Botones para actualizar y eliminar */}
                         <button onClick={() => prepararActualizarEquipo(equipo)}>Actualizar</button>
                     </li>
                 ))}
             </ul>
+
             {/* Campo para ingresar el ID del equipo a eliminar */}
             <div>
                 <input
                     type="text"
-                    name="idEquipoEliminar"
-                    value={idEquipoEliminar.idcod}
+                    value={idEquipoEliminar}
                     onChange={handleChange}
                     placeholder="Ingrese el ID del equipo a eliminar"
                 />
