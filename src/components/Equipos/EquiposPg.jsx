a37daa8af85df5b2938df93909641768e5e69a6e
nicolemotoche29 <fernandamotoche@gmail.com>
2024-07-26 16:28:33 -0500

    Equipos

--- src/components/Equipos/EquiposPg.jsx
+++ src/components/Equipos/EquiposPg.jsx
@@ -165,193 +165,217 @@ const EquiposPg = () => {
 
             {/* Formulario para crear un nuevo equipo */}
             <form className="equipos-form" onSubmit={crearNuevoEquipo}>
-                <input
-                    type="text"
-                    name="idcod"
-                    value={nuevoEquipo.idcod}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, idcod: e.target.value })}
-                    placeholder="Código ID"
-                    required
-                />
-                <input
-                    type="text"
-                    name="descripcion"
-                    value={nuevoEquipo.descripcion}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, descripcion: e.target.value })}
-                    placeholder="Descripción"
-                    required
-                />
-                <input
-                    type="text"
-                    name="marca"
-                    value={nuevoEquipo.marca}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, marca: e.target.value })}
-                    placeholder="Marca"
-                    required
-                />
-                <input
-                    type="text"
-                    name="modelos"
-                    value={nuevoEquipo.modelos}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, modelos: e.target.value })}
-                    placeholder="Modelos"
-                    required
-                />
-                <input
-                    type="text"
-                    name="nserie"
-                    value={nuevoEquipo.nserie}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, nserie: e.target.value })}
-                    placeholder="Nº de serie"
-                    required
-                />
-                <input
-                    type="text"
-                    name="accesorios"
-                    value={nuevoEquipo.accesorios}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, accesorios: e.target.value })}
-                    placeholder="Accesorios"
-                    required
-                />
-                <input
-                    type="text"
-                    name="fabricante"
-                    value={nuevoEquipo.fabricante}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, fabricante: e.target.value })}
-                    placeholder="Fabricante"
-                    required
-                />
-                <input
-                    type="text"
-                    name="caracteristicas"
-                    value={nuevoEquipo.caracteristicas}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, caracteristicas: e.target.value })}
-                    placeholder="Caracteristicas"
-                    required
-                />
-                <input
-                    type="text"
-                    name="con_instalacion"
-                    value={nuevoEquipo.con_instalacion}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_instalacion: e.target.value })}
-                    placeholder="Instalación"
-                    required
-                />
-                <input
-                    type="text"
-                    name="con_utilizacion"
-                    value={nuevoEquipo.con_utilizacion}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_utilizacion: e.target.value })}
-                    placeholder="Utilización"
-                    required
-                />
-                <input
-                    type="text"
-                    name="area"
-                    value={nuevoEquipo.area}
-                    onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, area: e.target.value })}
-                    placeholder="Area"
-                    required
-                />
-                
-                <button type="submit">Crear Equipo</button>
-            </form>
-
-            <h2>Actualizar equipo</h2>
-            
-            {/* Formulario para actualizar un equipo */}
-            {mostrarFormularioActualizar && (
-                <form className="equipos-form" onSubmit={actualizarEquipo}>
+                <div className="form-row">
                     <input
                         type="text"
                         name="idcod"
-                        value={equipoActualizar.idcod}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.idcod}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, idcod: e.target.value })}
                         placeholder="Código ID"
                         required
                     />
                     <input
                         type="text"
                         name="descripcion"
-                        value={equipoActualizar.descripcion}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.descripcion}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, descripcion: e.target.value })}
                         placeholder="Descripción"
                         required
                     />
+                </div>
+                <div className="form-row">
                     <input
                         type="text"
                         name="marca"
-                        value={equipoActualizar.marca}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.marca}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, marca: e.target.value })}
                         placeholder="Marca"
                         required
                     />
                     <input
                         type="text"
                         name="modelos"
-                        value={equipoActualizar.modelos}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.modelos}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, modelos: e.target.value })}
                         placeholder="Modelos"
                         required
                     />
+                </div>
+                <div className="form-row">
                     <input
                         type="text"
                         name="nserie"
-                        value={equipoActualizar.nserie}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.nserie}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, nserie: e.target.value })}
                         placeholder="Nº de serie"
                         required
                     />
                     <input
                         type="text"
                         name="accesorios"
-                        value={equipoActualizar.accesorios}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.accesorios}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, accesorios: e.target.value })}
                         placeholder="Accesorios"
                         required
                     />
+                </div>
+                <div className="form-row">
                     <input
                         type="text"
                         name="fabricante"
-                        value={equipoActualizar.fabricante}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.fabricante}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, fabricante: e.target.value })}
                         placeholder="Fabricante"
                         required
                     />
                     <input
                         type="text"
                         name="caracteristicas"
-                        value={equipoActualizar.caracteristicas}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.caracteristicas}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, caracteristicas: e.target.value })}
                         placeholder="Caracteristicas"
                         required
                     />
+                </div>
+                <div className="form-row">
                     <input
                         type="text"
                         name="con_instalacion"
-                        value={equipoActualizar.con_instalacion}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.con_instalacion}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_instalacion: e.target.value })}
                         placeholder="Instalación"
                         required
                     />
                     <input
                         type="text"
                         name="con_utilizacion"
-                        value={equipoActualizar.con_utilizacion}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.con_utilizacion}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, con_utilizacion: e.target.value })}
                         placeholder="Utilización"
                         required
                     />
+                </div>
+                <div className="form-row">
                     <input
                         type="text"
                         name="area"
-                        value={equipoActualizar.area}
-                        onChange={handleChangeActualizar}
+                        value={nuevoEquipo.area}
+                        onChange={(e) => setNuevoEquipo({ ...nuevoEquipo, area: e.target.value })}
                         placeholder="Area"
                         required
                     />
-                    
-                    <button type="submit">Actualizar Equipo</button>
+                </div>
+                <div className="button-container">
+                    <button type="submit">Crear Equipo</button>
+                </div>
+            </form>
+
+            {/* Formulario para actualizar un equipo */}
+            {mostrarFormularioActualizar && (
+                <form className="equipos-form" onSubmit={actualizarEquipo}>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="idcod"
+                            value={equipoActualizar.idcod}
+                            onChange={handleChangeActualizar}
+                            placeholder="Código ID"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="descripcion"
+                            value={equipoActualizar.descripcion}
+                            onChange={handleChangeActualizar}
+                            placeholder="Descripción"
+                            required
+                        />
+                    </div>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="marca"
+                            value={equipoActualizar.marca}
+                            onChange={handleChangeActualizar}
+                            placeholder="Marca"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="modelos"
+                            value={equipoActualizar.modelos}
+                            onChange={handleChangeActualizar}
+                            placeholder="Modelos"
+                            required
+                        />
+                    </div>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="nserie"
+                            value={equipoActualizar.nserie}
+                            onChange={handleChangeActualizar}
+                            placeholder="Nº de serie"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="accesorios"
+                            value={equipoActualizar.accesorios}
+                            onChange={handleChangeActualizar}
+                            placeholder="Accesorios"
+                            required
+                        />
+                    </div>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="fabricante"
+                            value={equipoActualizar.fabricante}
+                            onChange={handleChangeActualizar}
+                            placeholder="Fabricante"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="caracteristicas"
+                            value={equipoActualizar.caracteristicas}
+                            onChange={handleChangeActualizar}
+                            placeholder="Caracteristicas"
+                            required
+                        />
+                    </div>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="con_instalacion"
+                            value={equipoActualizar.con_instalacion}
+                            onChange={handleChangeActualizar}
+                            placeholder="Instalación"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="con_utilizacion"
+                            value={equipoActualizar.con_utilizacion}
+                            onChange={handleChangeActualizar}
+                            placeholder="Utilización"
+                            required
+                        />
+                    </div>
+                    <div className="form-row">
+                        <input
+                            type="text"
+                            name="area"
+                            value={equipoActualizar.area}
+                            onChange={handleChangeActualizar}
+                            placeholder="Area"
+                            required
+                        />
+                    </div>
+                    <div className="button-container">
+                        <button type="submit">Actualizar Equipo</button>
+                    </div>
                 </form>
             )}
 
@@ -374,7 +398,9 @@ const EquiposPg = () => {
                     onChange={handleChange}
                     placeholder="Ingrese el ID del equipo a eliminar"
                 />
-                <button onClick={eliminarEquipo}>Eliminar Eq </button>
+                <div className="button-container">
+                    <button onClick={eliminarEquipo}>Eliminar Equipos</button>
+                </div>
             </div>
         </div>
     );
